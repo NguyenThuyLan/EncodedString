3546c7f3ce07ed52d2a58035f0861cc09c37a80c
Lan Nguyen Thuy <lnt@umbraco.dk>
2024-10-07 17:41:56 +0700

    init ecodedstring project

---
+++ App_Plugins/EncodedString/src/string-encoding-property-editor-ui.element.ts
@@ -0,0 +1,55 @@
+﻿import { LitElement, html, customElement, property, css } from "@umbraco-cms/backoffice/external/lit";
+import { UmbPropertyEditorUiElement } from "@umbraco-cms/backoffice/extension-registry";
+import { UmbPropertyValueChangeEvent } from "@umbraco-cms/backoffice/property-editor";
+
+@customElement('string-encoding-property-editor-ui')
+export default class StringEncodingPropertyEditorUIElement extends LitElement implements UmbPropertyEditorUiElement {
+    @property({ type: String })
+    public value = "";
+
+    @property({ type: String })
+    public displayValue = "";
+
+    #onInput(e: InputEvent) {
+        e.stopPropagation();
+        const target = e.target as HTMLInputElement;
+        this.displayValue = target.value as string;
+        this.#dispatchChangeEvent();
+    }
+
+    #dispatchChangeEvent() {
+        this.value = btoa(this.displayValue);
+        
+        this.dispatchEvent(new UmbPropertyValueChangeEvent());
+    }
+
+    async firstUpdated(changed: any) {
+        super.firstUpdated(changed)
+        this.displayValue = atob(this.value || '');
+    }
+
+    render() {
+        return html`
+            <uui-input
+                class="element"
+                .value=${this.displayValue || ""}
+                @input=${this.#onInput}
+            >
+            </uui-input>
+            `;
+    }
+
+    static styles = [
+        css`
+            .element {
+              width: 100%;
+            }
+          `,
+        ];
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'string-encoding-property-editor-ui': StringEncodingPropertyEditorUIElement;
+    }
+}
