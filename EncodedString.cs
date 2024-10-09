31dd1239b87b12e446e321d165019766adfada7e
Lan Nguyen Thuy <lnt@umbraco.dk>
2024-10-09 14:39:45 +0700

    add converter

---
+++ EncodedString.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+namespace EncodedString
+{
+	public class EncodedString
+	{
+		public string Value { get; init; }
+		public string DecodedValue => Encoding.UTF8.GetString(Convert.FromBase64String(Value));
+	}
+}
---
+++ EncodedStringValueConverter.cs
@@ -0,0 +1,40 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors;
+
+namespace EncodedString
+{
+	public class EncodedStringValueConverter : IPropertyValueConverter
+	{
+		public object? ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object? inter, bool preview)
+		{
+			return inter as EncodedString;
+		}
+
+		public object? ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object? source, bool preview)
+		{
+			return new EncodedString()
+			{
+				Value = source as string
+			};
+		}
+
+		public PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType)
+		{
+			return PropertyCacheLevel.Element;
+		}
+
+		public Type GetPropertyValueType(IPublishedPropertyType propertyType)
+		{
+			return typeof(EncodedString);
+		}
+
+		public bool IsConverter(IPublishedPropertyType propertyType)
+		{
+			return propertyType.EditorUiAlias.Equals("EncodedString");
+		}
+		public bool? IsValue(object? value, PropertyValueLevel level)
+		{
+			return !string.IsNullOrWhiteSpace(value as string);
+		}
+	}
+}
